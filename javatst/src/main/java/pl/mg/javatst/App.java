633625a6376122f5f3025a578328c4d43c554482
smieci000 <smieci000@gmail.com>
2015-02-20 20:04:34 +0100

    test zmiany

--- javatst/src/main/java/pl/mg/javatst/App.java
+++ javatst/src/main/java/pl/mg/javatst/App.java
@@ -4,10 +4,11 @@ package pl.mg.javatst;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
     }
+    
+    
 }
