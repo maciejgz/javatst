c15989a2042956e9a77ae55d77f92ddb45efa86c
smieci000 <smieci000@gmail.com>
2015-02-21 15:44:31 +0100

    join branch init

---
+++ javatst/src/main/java/pl/mg/javatst/threads/JoinTest.java
@@ -0,0 +1,24 @@
+package pl.mg.javatst.threads;
+
+/**
+ * join() test
+ * @author m
+ *
+ */
+public class JoinTest {
+
+    public static void main(String[] args) {
+        Thread th1 = new Thread(new SomeThread("1"));
+
+        Thread th2 = new Thread(new SomeThread("2"));
+        th1.start();
+        try {
+            th1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("after join");
+
+    }
+
+}
---
+++ javatst/src/main/java/pl/mg/javatst/threads/SomeThread.java
@@ -0,0 +1,23 @@
+package pl.mg.javatst.threads;
+
+import java.util.Random;
+
+public class SomeThread implements Runnable {
+
+    private String id;
+
+    public SomeThread(String id) {
+        this.id = id;
+    }
+
+    public void run() {
+        System.out.println("thread_" + id + "-start");
+        try {
+            Thread.sleep((new Random()).nextInt(5000));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("thread_" + id + "-stop");
+    }
+
+}
