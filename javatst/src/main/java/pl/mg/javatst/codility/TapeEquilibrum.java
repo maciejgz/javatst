6f8bde0dc68525fbffdb3edcc649122a29b59f1a
mgzik <mgzik@ospartner.pl>
2018-04-08 20:17:28 +0200

    Codility: Lesson 3.1 25%

--- javatst/src/main/java/pl/mg/javatst/codility/TapeEquilibrum.java
+++ javatst/src/main/java/pl/mg/javatst/codility/TapeEquilibrum.java
@@ -1,74 +1,85 @@
 package pl.mg.javatst.codility;
 
 /**
- * A non-empty zero-indexed array A consisting of N integers is given. Array A represents numbers on a tape.
- * <p/>
- * Any integer P, such that 0 < P < N, splits this tape into two non-empty parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
- * <p/>
- * The difference between the two parts is the value of: |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
- * <p/>
- * In other words, it is the absolute difference between the sum of the first part and the sum of the second part.
- * <p/>
+ * A non-empty zero-indexed array A consisting of N integers is given.
+ * Array A represents numbers on a tape.
+ * <p>
+ * Any integer P, such that 0 < P < N, splits this tape into two non-empty
+ * parts: A[0], A[1], ..., A[P − 1] and A[P], A[P + 1], ..., A[N − 1].
+ * <p>
+ * The difference between the two parts is the value of:
+ * |(A[0] + A[1] + ... + A[P − 1]) − (A[P] + A[P + 1] + ... + A[N − 1])|
+ * <p>
+ * In other words, it is the absolute difference between
+ * the sum of the first part and the sum of the second part.
+ * <p>
  * For example, consider array A such that:
- * <p/>
+ * <p>
  * A[0] = 3
  * A[1] = 1
  * A[2] = 2
  * A[3] = 4
  * A[4] = 3
  * We can split this tape in four places:
- * <p/>
+ * <p>
  * P = 1, difference = |3 − 10| = 7
  * P = 2, difference = |4 − 9| = 5
  * P = 3, difference = |6 − 7| = 1
  * P = 4, difference = |10 − 3| = 7
  * Write a function:
- * <p/>
+ * <p>
  * class Solution { public int solution(int[] A); }
- * <p/>
- * that, given a non-empty zero-indexed array A of N integers, returns the minimal difference that can be achieved.
- * <p/>
+ * <p>
+ * that, given a non-empty zero-indexed array A of N integers,
+ * returns the minimal difference that can be achieved.
+ * <p>
  * For example, given:
- * <p/>
+ * <p>
  * A[0] = 3
  * A[1] = 1
  * A[2] = 2
  * A[3] = 4
  * A[4] = 3
  * the function should return 1, as explained above.
- * <p/>
+ * <p>
  * Assume that:
- * <p/>
+ * <p>
  * N is an integer within the range [2..100,000];
  * each element of array A is an integer within the range [−1,000..1,000].
  * Complexity:
- * <p/>
+ * <p>
  * expected worst-case time complexity is O(N);
- * expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
- * Elements of input arrays can be modified.
- * Created by Maciej on 2015-03-09.
- *
- *  Done
- * 100%
+ * expected worst-case space complexity is O(N), beyond input
+ * storage (not counting the storage required for input arguments).
+ * <p>
  */
 public class TapeEquilibrum {
 
+    public static void main(String[] args) {
+        int[] testArray = {3, 1, 2, 4, 3};
+
+        TapeEquilibrum tapeEquilibrum = new TapeEquilibrum();
+        int solution = tapeEquilibrum.solution(testArray);
+    }
+
     public int solution(int[] A) {
         int sumRight = 0;
-        for (int i = 0; i < A.length; i++) {
+        for (int i = 1; i < A.length; i++) {
             sumRight += A[i];
         }
+
         int sumLeft = A[0];
-        int answer = Math.abs(sumLeft = sumRight);
+        int minimumAnswer = Math.abs(sumLeft - sumRight);
+        int answer;
         for (int i = 1; i < A.length; i++) {
-            if (Math.abs(sumLeft - sumRight) < answer) {
-                answer = Math.abs(sumLeft - sumRight);
+
+            answer = Math.abs(sumLeft - sumRight);
+            if (answer < minimumAnswer) {
+                minimumAnswer = answer;
             }
-
-            sumLeft += A[i];
-            sumRight -= A[i];
+            sumLeft += A[i - 1];
+            sumRight -= A[i - 1];
         }
-
-        return answer;
+        return minimumAnswer;
     }
 }
