a1de75d74172ff64bb0856eee9e2c1f7b4c6a83b
maciejgz <macgzi@gmail.com>
2018-04-09 19:55:36 +0200

    Codility: lesson 3.3

--- javatst/src/main/java/pl/mg/javatst/codility/FrogJump.java
+++ javatst/src/main/java/pl/mg/javatst/codility/FrogJump.java
@@ -1,9 +1,8 @@
 package pl.mg.javatst.codility;
 
 /**
- *
  * https://app.codility.com/programmers/lessons/3-time_complexity/
- *
+ * <p>
  * A small frog wants to get to the other side of the road.
  * The frog is currently located at position X and wants to get to a
  * position greater than or equal to Y. The small frog always jumps a fixed distance, D.
@@ -37,8 +36,8 @@ package pl.mg.javatst.codility;
  * expected worst-case space complexity is O(1).
  * Created by Maiej on 2015-03-10.
  * <p>
- *
- *     100%
+ * <p>
+ * 100%
  */
 public class FrogJump {
 
