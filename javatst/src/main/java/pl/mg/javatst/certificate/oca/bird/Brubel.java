1c0f442a407a31f181855fa2dd88c88c154b8bf5
maciejgz <macgzi@gmail.com>
2018-06-10 21:28:45 +0200

    some OCA tests

--- javatst/src/main/java/pl/mg/javatst/certificate/oca/bird/Brubel.java
+++ javatst/src/main/java/pl/mg/javatst/certificate/oca/bird/Brubel.java
@@ -1,8 +1,27 @@
 package pl.mg.javatst.certificate.oca.bird;
 
+import java.time.LocalDate;
+
 public class Brubel extends AbstractBird {
+
+  public static void main(String[] args) {
+
+    System.out.println("main");
+    StringBuilder b1 = new StringBuilder("snorkler");
+    StringBuilder b2 = new StringBuilder("yoodler");
+
+    b1.replace(3, 4, b2.substring(4)).append(b2.append(false));
+
+    System.out.println(b1.toString());
+    System.out.println(b2.toString());
+  }
 
   public void fly() {
     System.out.println("brubel flight");
+  }
+
+  public void throwExceptionMethodTest() throws BrubelException {
+
+    LocalDate date;
   }
 }
---
+++ javatst/src/main/java/pl/mg/javatst/certificate/oca/bird/BrubelException.java
@@ -0,0 +1,4 @@
+package pl.mg.javatst.certificate.oca.bird;
+
+public class BrubelException extends Exception {
+}
