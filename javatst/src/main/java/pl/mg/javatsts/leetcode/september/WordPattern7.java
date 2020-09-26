c67ef14bae0a7c1c2b7350119dd421c9fcfde482
maciejgz <macgzi@gmail.com>
2020-09-26 09:11:59 +0200

    leetcode - task 7

---
+++ javatst/src/main/java/pl/mg/javatsts/leetcode/september/WordPattern7.java
@@ -0,0 +1,40 @@
+package pl.mg.javatsts.leetcode.september;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+public class WordPattern7 {
+
+    public static void main(String[] args) {
+        WordPattern7 pattern7 = new WordPattern7();
+        System.out.println(pattern7.wordPattern("abba", "dog dog dog dog"));
+    }
+
+    public boolean wordPattern(String pattern, String s) {
+        String[] values = s.split(" ");
+        char[] patternIds = pattern.toCharArray();
+        if (patternIds.length == 0 || patternIds.length != values.length) {
+            return false;
+        }
+        Map<Character, String> patterns = new HashMap<>();
+        int i = 0;
+        for (char patternId : patternIds) {
+            if (patterns.containsKey(patternId)
+                    && !values[i].equals(patterns.get(patternId))) {
+                return false;
+            }
+            for (Character character : patterns.keySet()) {
+                if (patternId != character && patterns.get(character).equals(values[i])) {
+                    return false;
+                }
+            }
+            patterns.put(patternId, values[i]);
+            i++;
+        }
+        return true;
+    }
+}
