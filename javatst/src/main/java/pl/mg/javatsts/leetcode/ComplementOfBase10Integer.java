8d572b2af3032ed4c90effcea3fe6549a7f56077
maciejgz <macgzi@gmail.com>
2020-10-05 18:04:45 +0200

    leetcode - tasks 05.10

---
+++ javatst/src/main/java/pl/mg/javatsts/leetcode/ComplementOfBase10Integer.java
@@ -0,0 +1,23 @@
+package pl.mg.javatsts.leetcode;
+
+/**
+ * https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/559/week-1-october-1st-october-7th/3484/
+ * Every non-negative integer N has a binary representation.  For example, 5 can be represented as "101" in binary, 11
+ * as "1011" in binary, and so on.  Note that except for N = 0, there are no leading zeroes in any binary representation.
+ * <p>
+ * The complement of a binary representation is the number in binary you get when changing every 1 to a 0 and 0 to a 1.
+ * For example, the complement of "101" in binary is "010" in binary.
+ * <p>
+ * For a given number N in base-10, return the complement of it's binary representation as a base-10 integer.
+ */
+public class ComplementOfBase10Integer {
+
+    public int bitwiseComplement(int N) {
+        String s = Integer.toBinaryString(N);
+        StringBuilder result = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            result.append(c == '1' ? '0' : '1');
+        }
+        return Integer.parseInt(result.toString(), 2);
+    }
+}
---
+++ javatst/src/main/java/pl/mg/javatsts/leetcode/PrefixWord.java
@@ -0,0 +1,31 @@
+package pl.mg.javatsts.leetcode;
+
+/**
+ * https://leetcode.com/problems/check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/
+ * <p>
+ * Given a sentence that consists of some words separated by a single space, and a searchWord.
+ * <p>
+ * You have to check if searchWord is a prefix of any word in sentence.
+ * <p>
+ * Return the index of the word in sentence where searchWord is a prefix of this word (1-indexed).
+ * <p>
+ * If searchWord is a prefix of more than one word, return the index of the first word (minimum index). If there is no such word return -1.
+ * <p>
+ * A prefix of a string S is any leading contiguous substring of S.
+ */
+public class PrefixWord {
+
+    public int isPrefixOfWord(String sentence, String searchWord) {
+        int index = 1;
+        boolean found = false;
+        for (String s : sentence.split(" ")) {
+            if (s.startsWith(searchWord)) {
+                found = true;
+                break;
+            }
+            index++;
+        }
+        return found ? index : -1;
+    }
+
+}
