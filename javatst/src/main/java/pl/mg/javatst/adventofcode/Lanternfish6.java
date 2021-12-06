3b441e92381d6fd8e681d22f2e5f4f53a98b49d2
maciejgz <macgzi@gmail.com>
2021-12-06 19:37:35 +0100

    Day 6 part two

--- javatst/src/main/java/pl/mg/javatst/adventofcode/Lanternfish6.java
+++ javatst/src/main/java/pl/mg/javatst/adventofcode/Lanternfish6.java
@@ -12,6 +12,8 @@ public class Lanternfish6 {
             List<String> input = FileUtil.readLines("adventofcode//6.txt");
             int result = simulate80Days(input);
             System.out.println("after 80 days: " + result);
+            long result256 = simulate256Days(input);
+            System.out.println("after 256 days: " + result256);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -21,19 +23,60 @@ public class Lanternfish6 {
         List<Lanternfish> fishes = Arrays.stream(input.get(0).split(","))
                 .map(state -> new Lanternfish(Integer.parseInt(state))).collect(Collectors.toList());
         System.out.println(fishes.size());
-        for (int i = 0; i < 18; i++) {
+        for (int i = 0; i < 80; i++) {
+            int newFishes = 0;
             for (int j = 0; j < fishes.size(); j++) {
                 if (fishes.get(j).processDay()) {
-                    fishes.add(new Lanternfish(8));
+                    newFishes++;
+                }
+            }
+            if (i != 79) {
+                for (int j = 0; j < newFishes; j++) {
+                    fishes.add(new Lanternfish(9));
                 }
             }
         }
         return fishes.size();
     }
 
+    private static long simulate256Days(List<String> input) {
+        List<Lanternfish> fishes = Arrays.stream(input.get(0).split(","))
+                .map(state -> new Lanternfish(Integer.parseInt(state))).collect(Collectors.toList());
+        int result = 0;
+        long[] fishesGrouped = new long[9];
+        for (Lanternfish fish : fishes) {
+            fishesGrouped[fish.state]++;
+        }
+
+        for (int i = 0; i < 256; i++) {
+            long temp = fishesGrouped[0];
+            fishesGrouped[0] = fishesGrouped[1];
+            fishesGrouped[1] = fishesGrouped[2];
+            fishesGrouped[2] = fishesGrouped[3];
+            fishesGrouped[3] = fishesGrouped[4];
+            fishesGrouped[4] = fishesGrouped[5];
+            fishesGrouped[5] = fishesGrouped[6];
+            fishesGrouped[6] = fishesGrouped[7] + temp;
+            fishesGrouped[7] = fishesGrouped[8];
+
+            if (i != 0) {
+                fishesGrouped[8] = temp;
+            }
+        }
+
+        return fishesGrouped[0] +
+                fishesGrouped[1] +
+                fishesGrouped[2] +
+                fishesGrouped[3] +
+                fishesGrouped[4] +
+                fishesGrouped[5] +
+                fishesGrouped[6] +
+                fishesGrouped[7] +
+                fishesGrouped[8];
+    }
+
     private static class Lanternfish {
         public int state;
-
 
         public Lanternfish(int state) {
             this.state = state;
@@ -42,7 +85,7 @@ public class Lanternfish6 {
         public boolean processDay() {
             this.state--;
             if (state == 0) {
-                state = 6;
+                state = 7;
                 return true;
             } else {
                 return false;
