1b5bdb06d67d51ace662a1083f5e4ecd7fde6ad3
maciejgz <macgzi@gmail.com>
2022-07-10 14:43:47 +0200

    sort binary tree by levels

---
+++ javatst/src/main/java/pl/mg/javatst/codewars/ListingBinaryTree.java
@@ -0,0 +1,16 @@
+package pl.mg.javatst.codewars;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Sort binary tree by levels kata
+ */
+public class ListingBinaryTree {
+
+    public static List<Integer> treeByLevels(Node node)
+    {
+        List<Integer> result = new ArrayList<>();
+        return null;
+    }
+}
---
+++ javatst/src/main/java/pl/mg/javatst/codewars/Node.java
@@ -0,0 +1,13 @@
+package pl.mg.javatst.codewars;
+
+public class Node {
+    public Node left;
+    public Node right;
+    public int value;
+
+    public Node(Node l, Node r, int v) {
+        left = l;
+        right = r;
+        value = v;
+    }
+}
