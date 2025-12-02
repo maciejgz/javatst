2684db709096fc8b61d3d5c38a525ba47453f7a2
maciejgz <macgzi@gmail.com>
2025-12-02 13:03:16 +0100

    LRUCache implementation

---
+++ src/main/java/pl/mg/javatst/leetcode/LFUCache.java
@@ -0,0 +1,9 @@
+package pl.mg.javatst.leetcode;
+
+public class LFUCache {
+
+    static void main() {
+        // TODO implement LFUCache
+
+    }
+}
--- src/main/java/pl/mg/javatst/leetcode/LRUCache.java
+++ src/main/java/pl/mg/javatst/leetcode/LRUCache.java
@@ -3,32 +3,36 @@ package pl.mg.javatst.leetcode;
 import java.util.LinkedHashMap;
 
 public class LRUCache {
-
     private final LinkedHashMap<Integer, Integer> cache;
 
-    public static void main(String[] args) {
+    static void main() {
         LRUCache lRUCache = new LRUCache(2);
         lRUCache.put(1, 1); // cache is {1=1}
         lRUCache.put(2, 2); // cache is {1=1, 2=2}
-        lRUCache.get(1);    // return 1
+        System.out.println(lRUCache.get(1));    // return 1
         lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
-        lRUCache.get(2);    // returns -1 (not found)
+        System.out.println(lRUCache.get(2));    // returns -1 (not found)
         lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
-        lRUCache.get(1);    // return -1 (not found)
-        lRUCache.get(3);    // return 3
-        lRUCache.get(4);    // return 4
+        System.out.println(lRUCache.get(1));    // return -1 (not found)
+        System.out.println(lRUCache.get(3));    // return 3
+        System.out.println(lRUCache.get(4));    // return 4
     }
 
     public LRUCache(int capacity) {
-        this.cache = new LinkedHashMap<>(capacity);
+        this.cache = new LinkedHashMap<>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(java.util.Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
     }
 
     public int get(int key) {
-        return this.cache.getOrDefault(key, -1);
+        return cache.getOrDefault(key, -1);
     }
 
     public void put(int key, int value) {
-        this.cache.get(key);
+        cache.put(key, value);
     }
 
 }
