d8aca91589ebc4247d5be20928440dc4f2d95b07
mgzik <mgzik@eversis.com>
2025-12-03 12:25:32 +0100

    implement initial structure for LFUCache with Node and FrequencyNode classes

--- src/main/java/pl/mg/javatst/leetcode/LFUCache.java
+++ src/main/java/pl/mg/javatst/leetcode/LFUCache.java
@@ -1,9 +1,30 @@
 package pl.mg.javatst.leetcode;
 
+import java.util.HashMap;
+
 public class LFUCache {
 
-    static void main() {
-        // TODO implement LFUCache
+    private HashMap<Integer, Node> byKey = new HashMap<>();
+    private HashMap<Integer, FrequencyNode> byFreq = new HashMap<>();
 
+    static void main() {
+
+    }
+
+
+
+    private class Node {
+        int key;
+        int value;
+        int frequency = 1;
+        Node prev, next;
+
+    }
+
+    private class FrequencyNode {
+        int frequency;
+
+        FrequencyNode prev, next;
+        Node itemsHead, itemsTail;
     }
 }
