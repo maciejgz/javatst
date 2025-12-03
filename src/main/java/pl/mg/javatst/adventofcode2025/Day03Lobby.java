7ac59f256c31907a33abab608fed28f1ca22a28d
maciejgz <macgzi@gmail.com>
2025-12-03 20:44:33 +0100

    add Day 3 Lobby implementation and input file

---
+++ src/main/java/pl/mg/javatst/adventofcode2025/Day03Lobby.java
@@ -0,0 +1,13 @@
+package pl.mg.javatst.adventofcode2025;
+
+import pl.mg.javatst.adventofcode2024.Utils;
+
+import java.util.List;
+
+public class Day03Lobby {
+
+    static void main() {
+        List<String> lines = Utils.readLines("src/main/resources/adventofcode2025/day03_input.txt");
+
+    }
+}
